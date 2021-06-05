cb51fe7036f963ce3dbb69feb12f58535ca14971
sarkaju <85273682+sarkaju@users.noreply.github.com>
2021-06-05 10:20:35 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-# This file is part of Daisy (Telegram Bot)
+# This file is part of SARKAR (Telegram Bot)
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -20,10 +20,10 @@ from importlib import import_module
 from aiogram import executor
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
-from DaisyX import TOKEN, bot, dp
-from DaisyX.config import get_bool_key, get_list_key
-from DaisyX.modules import ALL_MODULES, LOADED_MODULES, MOD_HELP
-from DaisyX.utils.logger import log
+from SARKAR import TOKEN, bot, dp
+from SARKAR.config import get_bool_key, get_list_key
+from SARKAR.modules import ALL_MODULES, LOADED_MODULES, MOD_HELP
+from SARKAR.utils.logger import log
 
 if get_bool_key("DEBUG_MODE"):
     log.debug("Enabling logging middleware.")
@@ -46,7 +46,7 @@ if get_bool_key("LOAD_MODULES"):
         if module_name == "pm_menu":
             continue
         log.debug(f"Importing <d><n>{module_name}</></>")
-        imported_module = import_module("DaisyX.modules." + module_name)
+        imported_module = import_module("SARKAR.modules." + module_name)
         if hasattr(imported_module, "__help__"):
             if hasattr(imported_module, "__mod_name__"):
                 MOD_HELP[imported_module.__mod_name__] = imported_module.__help__
@@ -59,11 +59,11 @@ else:
 
 loop = asyncio.get_event_loop()
 
-import_module("DaisyX.modules.pm_menu")
+import_module("SARKAR.modules.pm_menu")
 # Import misc stuff
-import_module("DaisyX.utils.exit_gracefully")
+import_module("SARKAR.utils.exit_gracefully")
 if not get_bool_key("DEBUG_MODE"):
-    import_module("DaisyX.utils.sentry")
+    import_module("SARKAR.utils.sentry")
 
 
 async def before_srv_task(loop):
